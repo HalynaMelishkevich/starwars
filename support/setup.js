5815b3ae545d7d9130636aa682f9f57629140001
halka <melishkevich.gala@gmail.com>
2019-06-17 16:14:18 +0300

    Allure added

--- support/setup.js
+++ support/setup.js
@@ -8,29 +8,21 @@ global.request = request(baseUrl);
 
 global.retryCount = 2;
 global.testData = require('../support/testData');
-/**
- * Logging is done with winston library
- * Possible logging levels:
- * { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 }
- * Passing the logLevel from command line. Use it like: logLevel=debug npm test
- * Default logLevel = error
- */
-const winston = require('winston');
-const logLevel = process.env.logLevel;
-winston.level = logLevel || 'error';
-global.logger = winston.createLogger({
-    transports: [
-        new winston.transports.Console({
-            level: logLevel,
-            prettyPrint: true,
-            colorize: true,
-            silent: false,
-            timestamp: false
+
+const { createLogger, format, transports } = require('winston');
+global.logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
         }),
-        new winston.transports.File({
-            filename: './logs/' + Date.now() + '.log',
-            level: 'debug',
-            prettyPrint: true
-        })
+        format.errors({ stack: true }),
+        format.splat(),
+        format.json()
+    ),
+    defaultMeta: { service: 'starwars' },
+    transports: [
+        new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.File({ filename: 'info.log' })
     ]
 });
--- test/root.js
+++ test/root.js
@@ -2,8 +2,10 @@ const { getAllEndpoints } = require('../support/api/root/getRoot');
 
 describe(`Tests for base endpoint`, async () => {
     it(`@01 Should get correct number of endpoints`, async () => {
+        allure.feature('Root');
         const result = await getAllEndpoints();
-        logger.log(result.body);
+        logger.log('info', `Response on base URL request:\n${JSON.stringify(result)}`);
+
         result.status.should.equal(200);
         assert.deepEqual(testData.expectedEndpoints.length, result.body.length, `Number of endpoints should be ${testData.expectedEndpoints.length}`);
     });
